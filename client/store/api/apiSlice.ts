29704e20eb30fad9082bcc276c53c108ede16fbc
anujnema5 <anujnema63@gmail.com>
2024-03-21 16:32:33 +0530

    connection error fix

--- client/store/api/apiSlice.ts
+++ client/store/api/apiSlice.ts
@@ -1,9 +1,9 @@
 import { logout, setCredentials } from '@/features/userSlice'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { headers } from 'next/headers'
+import 'dotenv/config'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api/',
+    baseUrl: process.env.SERVER_URL,
     credentials: 'include',
     prepareHeaders: (headers, {getState}: {getState: any})=> {
         const token = getState().user.token
@@ -20,14 +20,11 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 
     if (result?.error?.originalStatus === 403) {
         console.log('sending refresh token')
-        // send refresh token to get new access token 
         const refreshResult = await baseQuery('/refresh', api, extraOptions) as any
         
         if (refreshResult?.data) {
             const user = api.getState().auth.user
-            // store the new token 
             api.dispatch(setCredentials({ ...refreshResult.data, user }))
-            // retry the original query with new access token 
             result = await baseQuery(args, api, extraOptions)
         } else {
             api.dispatch(logout())
