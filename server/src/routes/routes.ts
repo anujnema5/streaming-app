b891794be402fda9b1dfcce749b0a78d35893aee
anujnema5 <anujnema63@gmail.com>
2024-03-26 20:34:21 +0530

    sone test 2 aws

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const routes = Router()
     .get('/', (req, res) => { return res.send("SERVER IS RUNNING") })
-    .get('/test', (req, res)=> {return res.send("TEST ROUTE")})
+    .get('/test', (req, res)=> {return res.json({message: "Hello World Test"})})
     .use(authRoutes)
 
 export default routes.use('/api/',  routes)
